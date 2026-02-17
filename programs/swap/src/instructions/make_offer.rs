27498168552bdab1d697324f7968f0915d2eeb40
Goodbadboy <selfmanademola@gmail.com>
2026-02-17 08:50:08 +0100

    I no understand shit again

--- programs/swap/src/instructions/make_offer.rs
+++ programs/swap/src/instructions/make_offer.rs
@@ -8,9 +8,26 @@ pub struct MakeOffer <'info> {
 
     #[account(mint::token_program = token_program)]
     pub token_mint_a: InterfaceAccount<'info, Mint>,
-    
+
     #[account(mint::token_program = token_program)]
     pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut, 
+        associated_token::mint = token_mint_a,
+        associated_token::authority = maker,
+        token::token_program = token_program
+    )]
+    pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init, 
+        payer = maker,
+        space = ANCHOR_DISCRIMINATOR + Offer::INIT_SPACE,
+        seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub offer: Account<'info, System>,
 }
 
 pub fn send_offered_tokens_to_vault(ctx: Context<MakeOffer>) -> Result<()> {
