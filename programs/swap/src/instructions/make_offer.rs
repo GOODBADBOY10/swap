451cd23c985130fcf350e359b5cc6fb402ffca41
Goodbadboy <selfmanademola@gmail.com>
2026-02-17 08:56:30 +0100

    everywhere just red

--- programs/swap/src/instructions/make_offer.rs
+++ programs/swap/src/instructions/make_offer.rs
@@ -1,7 +1,13 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::Mint;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+use crate::ANCHOR_DISCRIMINATOR;
 
 #[derive(Accounts)]
+#[instruction(id: u64)]
 pub struct MakeOffer <'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
@@ -27,7 +33,20 @@ pub struct MakeOffer <'info> {
         seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()],
         bump
     )]
-    pub offer: Account<'info, System>,
+    pub offer: Account<'info, Offer>,
+
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = offer,
+        token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 pub fn send_offered_tokens_to_vault(ctx: Context<MakeOffer>) -> Result<()> {
